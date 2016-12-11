deebede09eaeb703f9f98070ef4103ecc96d6195
Denis Nek <den@revolut.com>
2016-12-11 15:44:06 +0300

    Removed retrofit2

--- app/src/main/kotlin/com/github/nekdenis/videoupload/di/NetworkModule.kt
+++ app/src/main/kotlin/com/github/nekdenis/videoupload/di/NetworkModule.kt
@@ -1,22 +1,10 @@
 package com.github.nekdenis.videoupload.di
 
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import dagger.Module
-import dagger.Provides
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import javax.inject.Singleton
 
 @Module
 class NetworkModule {
 
-    @Provides
-    @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-                .baseUrl("https://www.reddit.com")
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(MoshiConverterFactory.create())
-                .build()
-    }
+    //TODO: provide retrofit if necessary
+
 }
